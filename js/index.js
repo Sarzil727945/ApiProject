24a540a5fb1c2ace2ea5ac9042c4f99a4d118f23
Sarzil727945 <sarzilmuntaha.gmail.com>
2023-03-02 10:52:10 +0600

    edit add

--- js/index.js
+++ js/index.js
@@ -10,7 +10,6 @@ const loadUser = async(searchText) =>{
  function display(dataArray){
     const parentDiv = document.getElementById('parent-div');
     parentDiv.innerHTML = '';
-
      dataArray.forEach(element =>{
         //   new tag create 
           const childDiv = document.createElement('div');
@@ -25,7 +24,7 @@ const loadUser = async(searchText) =>{
                     <h5 class="card-title">${element.strArea}</h5>
                      <p class="card-text"> Put the potatoes in a large pan of cold salted water and bring to the boil. Lower the heat, cover, then simmer gently for 15 minutes until tender. Drain, then return to the pan over a low heat for</p>
                     <button onclick="modalJson('${element.idMeal}')" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                    Details Information
+                    Details
                   </button>
                 </div>
             </div>
@@ -59,9 +58,11 @@ loadUser('chi');
  function modal(arrayModal){
   const modalParentDiv = document.getElementById('modal-div');
   modalParentDiv.innerHTML = '';
+
   arrayModal.forEach(element =>{
     const divField = document.getElementById('exampleModalLabel');
     divField.innerText = element.strArea;
+
     const modalChildDiv = document.createElement('div');
     modalChildDiv.innerHTML =`
     <img class="img-fluid" src="${element.strMealThumb}"  alt="...">
